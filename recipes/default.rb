f2fbee15050479c501a189d6fde86404f175ace2
James McCormick <James.McCormick@progress.com>
2021-04-14 22:23:56 +0100

    added an ! to test webhook

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@ end
 
 file '/var/www/html/index.html' do
   action :create
-  content '<p><strong>HELLO</strong></p><p>&nbsp;</p><p><strong>this is a test page</strong></p><p>&nbsp;</p><p><strong><img src="https://html5-editor.net/tinymce/plugins/emoticons/img/smiley-cool.gif" alt="cool" /></strong></p>'
+  content '<p><strong>HELLO!</strong></p><p>&nbsp;</p><p><strong>this is a test page</strong></p><p>&nbsp;</p><p><strong><img src="https://html5-editor.net/tinymce/plugins/emoticons/img/smiley-cool.gif" alt="cool" /></strong></p>'
 end
 
 service 'apache2' do
